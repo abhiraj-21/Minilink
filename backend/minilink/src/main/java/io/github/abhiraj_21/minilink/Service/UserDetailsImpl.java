202eddca20600777141ff37d68cf773a7e64149b
Abhiraj <abhirajsinghchouhan4@gmail.com>
2025-08-13 12:50:57 +0530

    a custom UserDetailsImpl which acts as a bridge between entity and spring security

---
+++ backend/minilink/src/main/java/io/github/abhiraj_21/minilink/Service/UserDetailsImpl.java
@@ -0,0 +1,60 @@
+package io.github.abhiraj_21.minilink.Service;
+
+import io.github.abhiraj_21.minilink.Entities.User;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+//This class would act as a bridge between the User entity and Spring Security's UserDetails interface.
+
+@Data
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Long id, String username, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user){
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
