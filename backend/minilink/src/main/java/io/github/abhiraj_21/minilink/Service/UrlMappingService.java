c4bca07c3b182b1fdc0275bb2e2bd06d8d9dd72f
Abhiraj <abhirajsinghchouhan4@gmail.com>
2025-09-01 12:02:04 +0530

    complete backend for minilink

--- backend/minilink/src/main/java/io/github/abhiraj_21/minilink/Service/UrlMappingService.java
+++ backend/minilink/src/main/java/io/github/abhiraj_21/minilink/Service/UrlMappingService.java
@@ -1,5 +1,6 @@
 package io.github.abhiraj_21.minilink.Service;
 
+import io.github.abhiraj_21.minilink.Entities.ClickEvent;
 import io.github.abhiraj_21.minilink.Entities.UrlMapping;
 import io.github.abhiraj_21.minilink.Entities.User;
 import io.github.abhiraj_21.minilink.Repository.ClickEventRepository;
@@ -84,5 +85,18 @@ public class UrlMappingService {
         List<UrlMapping> userUrls = urlMappingRepository.findByUser(user);
         return clickEventRepository.findByUrlMappingInAndClickDateBetween(userUrls, start.atStartOfDay(), end.plusDays(1).atStartOfDay()).stream()
                 .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()));
+    }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping != null){
+            urlMapping.setClickCount(urlMapping.getClickCount()+1);
+            urlMappingRepository.save(urlMapping);
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+        return urlMapping;
     }
 }
