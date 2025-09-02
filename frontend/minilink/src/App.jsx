de02f933d4d6a751df7788763df3fd2f816f8577
Abhiraj <abhirajsinghchouhan4@gmail.com>
2025-09-02 08:18:20 +0530

    frontend project structure

---
+++ frontend/minilink/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-indigo-300">
+          <h1 className="font-bold text-2xl underline">Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
