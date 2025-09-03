78e006233d41712d31fbd732b60a3c39792365cf
Abhiraj <abhirajsinghchouhan4@gmail.com>
2025-09-03 12:27:13 +0530

    navbar and footer

--- frontend/minilink/src/App.jsx
+++ frontend/minilink/src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import LandingPage from "./Components/LandingPage.jsx";
 import AboutPage from "./Components/AboutPage.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
 
   return (
     <>
-        <BrowserRouter>
+        <Router>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/about" element={<AboutPage />} />
             </Routes>
-        </BrowserRouter>
+            <Footer />
+        </Router>
     </>
   )
 }
